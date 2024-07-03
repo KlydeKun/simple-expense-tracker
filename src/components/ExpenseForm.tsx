5841996c79b6752e047500fd39c849b5772fefe8
Kyle Lerio <kylelerio15@gmail.com>
2024-07-03 17:36:03 +0800

    Build expense form component and install prime react

---
+++ src/components/ExpenseForm.tsx
@@ -0,0 +1,16 @@
+interface FormProps {
+  firstName: string;
+  lastName: string;
+  age: number;
+  sex: string;
+}
+
+const ExpenseForm = () => {
+  return (
+    <div>
+      <h1>ExpenseForm</h1>
+    </div>
+  );
+};
+
+export default ExpenseForm;
