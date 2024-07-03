2b9e5c683b810b09ee42124021476d51ef193edd
Kyle Lerio <kylelerio15@gmail.com>
2024-07-03 18:27:49 +0800

    Build the expense form component and add themeswitcher component for dark mode

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,14 +1,49 @@
-interface FormProps {
-  firstName: string;
-  lastName: string;
-  age: number;
-  sex: string;
+import { InputText } from "primereact/inputtext";
+import { Dropdown, DropdownChangeEvent } from "primereact/dropdown";
+import { useState } from "react";
+import { Button } from "primereact/button";
+
+interface Gender {
+  name: string;
+  code: string;
 }
 
 const ExpenseForm = () => {
+  const [selectGender, setSelectGender] = useState<Gender | null>(null);
+  const genders: Gender[] = [
+    { name: "male", code: "m" },
+    { name: "female", code: "fm" },
+  ];
+
   return (
-    <div>
-      <h1>ExpenseForm</h1>
+    <div className="card flex flex-col gap-3">
+      <div className="flex flex-col gap-2">
+        <label htmlFor="firstName">First Name</label>
+        <InputText id="firstName" />
+      </div>
+      <div className="flex flex-col gap-2">
+        <label htmlFor="lastName">Last Name</label>
+        <InputText id="lastName" />
+      </div>
+      <div className="flex flex-col gap-2">
+        <label htmlFor="age">Age</label>
+        <InputText id="age" />
+      </div>
+      <div className="flex justify-between items-end">
+        <div className="flex flex-col gap-2">
+          <label htmlFor="gender">Gender</label>
+          <Dropdown
+            value={selectGender}
+            onChange={(e: DropdownChangeEvent) => setSelectGender(e.value)}
+            options={genders}
+            optionLabel="name"
+            placeholder="Select"
+          />
+        </div>
+        <div>
+          <Button label="Submit New User" />
+        </div>
+      </div>
     </div>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import App from "./App";
 import "./index.css";
 
+import "primeicons/primeicons.css";
 import { PrimeReactProvider } from "primereact/api";
-import "primereact/resources/primereact.css";
+import Tailwind from "primereact/passthrough/tailwind";
 import "primereact/resources/themes/lara-light-indigo/theme.css";
+import "primereact/resources/primereact.css";
+import ThemeSwitcher from "./components/ThemeSwitcher.tsx";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <PrimeReactProvider>
+    <PrimeReactProvider value={{ pt: Tailwind }}>
+      <ThemeSwitcher />
       <App />
     </PrimeReactProvider>
   </React.StrictMode>
