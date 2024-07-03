0b581efb1d05fa59ce70a84be1e7ec2480da8b29
KlydeKun <kylelerio15@gmail.com>
2024-07-04 02:23:34 +0800

    handle form submission and build a controlled component

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,56 +1,92 @@
 import { InputText } from "primereact/inputtext";
 import { Dropdown, DropdownChangeEvent } from "primereact/dropdown";
-import { useState } from "react";
+import { FormEvent, useState } from "react";
 import { Button } from "primereact/button";
-import { z } from "zod";
+// import { z } from "zod";
+// import { Message } from "primereact/message";
 
-interface Gender {
-  name: string;
+interface FormData {
+  firstName: string;
+  lastName: string;
+  age: string;
+  gender: string;
 }
 
-const select = ["male", "female"] as const;
+enum Gender {
+  Male = "male",
+  Female = "female",
+}
 
-z.object({
-  firstName: z.string().min(3).max(10),
-  lastName: z.string().min(3).max(10),
-  age: z.number().min(1).max(2),
-  gender: z.enum(select),
-});
+const genderOption = [
+  { label: "Male", value: Gender.Male },
+  { label: "Female", value: Gender.Female },
+];
+
+// const schema = z.object({
+//   firstName: z.string().min(3, "First name is required.").max(10),
+//   lastName: z.string().min(3, "Last name is required.").max(10),
+//   age: z.number().min(1, "Age is required.").max(128, "Invalid age"),
+//   gender: z.nativeEnum(Gender, { message: "Gender is required" }),
+// });
 
 const ExpenseForm = () => {
-  const [selectGender, setSelectGender] = useState<Gender | null>(null);
-  const genders: Gender[] = [{ name: "male" }, { name: "female" }];
+  const [person, setPerson] = useState<FormData>({
+    firstName: "",
+    lastName: "",
+    age: "",
+    gender: "",
+  });
+
+  const handleSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    console.log(person);
+  };
 
   return (
-    <div className="card flex flex-col gap-3">
+    <form className="card flex flex-col gap-3" onSubmit={handleSubmit}>
       <div className="flex flex-col gap-2">
         <label htmlFor="firstName">First Name</label>
-        <InputText id="firstName" />
+        <InputText
+          id="firstName"
+          type="text"
+          value={person.firstName}
+          onChange={(e) => setPerson({ ...person, firstName: e.target.value })}
+        />
       </div>
       <div className="flex flex-col gap-2">
         <label htmlFor="lastName">Last Name</label>
-        <InputText id="lastName" />
+        <InputText
+          id="lastName"
+          type="text"
+          onChange={(e) => setPerson({ ...person, lastName: e.target.value })}
+        />
       </div>
       <div className="flex flex-col gap-2">
         <label htmlFor="age">Age</label>
-        <InputText id="age" />
+        <InputText
+          id="age"
+          type="number"
+          onChange={(e) => setPerson({ ...person, age: e.target.value })}
+        />
       </div>
       <div className="flex justify-between items-end">
         <div className="flex flex-col gap-2">
           <label htmlFor="gender">Gender</label>
           <Dropdown
-            value={selectGender}
-            onChange={(e: DropdownChangeEvent) => setSelectGender(e.value)}
-            options={genders}
-            optionLabel="name"
+            id="gender"
+            value={person.gender}
+            onChange={(e: DropdownChangeEvent) => {
+              setPerson({ ...person, gender: e.value });
+            }}
+            options={genderOption}
             placeholder="Select"
           />
         </div>
         <div>
-          <Button label="Submit New User" />
+          <Button label="Submit New User" type="submit" />
         </div>
       </div>
-    </div>
+    </form>
   );
 };
 
