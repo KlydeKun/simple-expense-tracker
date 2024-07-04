4f286b75077560aecc6271ca8e106971c2883d59
Kyle Lerio <kylelerio15@gmail.com>
2024-07-04 15:32:44 +0800

    Apply validation using custom validation

--- src/components/ExpenseForm.tsx
+++ src/components/ExpenseForm.tsx
@@ -1,15 +1,9 @@
 import { InputText } from "primereact/inputtext";
 import { Dropdown } from "primereact/dropdown";
 import { Button } from "primereact/button";
-// import { z } from "zod";
+import { IconField } from "primereact/iconfield";
+import { InputIcon } from "primereact/inputicon";
 import { Controller, FieldValues, useForm } from "react-hook-form";
-
-// interface FormData {
-//   firstName: string;
-//   lastName: string;
-//   age: string;
-//   gender: string;
-// }
 
 enum Gender {
   Male = "male",
@@ -21,17 +15,19 @@ const genderOption = [
   { label: "Female", value: Gender.Female },
 ];
 
-// const schema = z.object({
-//   firstName: z.string().min(3, "First name is required.").max(10),
-//   lastName: z.string().min(3, "Last name is required.").max(10),
-//   age: z.number().min(1, "Age is required.").max(128, "Invalid age"),
-//   gender: z.nativeEnum(Gender, { message: "Gender is required" }),
-// });
-
 const ExpenseForm = () => {
-  const { register, handleSubmit, control } = useForm();
+  const {
+    register,
+    handleSubmit,
+    control,
+    reset,
+    formState: { errors },
+  } = useForm();
 
-  const onSubmit = (data: FieldValues) => console.log(data);
+  const onSubmit = (data: FieldValues) => {
+    console.log(data);
+    reset();
+  };
 
   return (
     <form
@@ -40,26 +36,107 @@ const ExpenseForm = () => {
     >
       <div className="flex flex-col gap-2">
         <label htmlFor="firstName">First Name</label>
-        <InputText
-          id="firstName"
-          type="text"
-          {...register("firstName")} // equivalent to value & onChange
-        />
+        <IconField>
+          {errors.firstName?.type === "required" && (
+            <InputIcon
+              className="pi pi-exclamation-circle"
+              style={{ color: "red" }}
+            />
+          )}
+          <InputText
+            id="firstName"
+            type="text"
+            {...register("firstName", { required: true, minLength: 3 })}
+            className={
+              errors.firstName?.type === "required" ||
+              errors.firstName?.type === "minLength"
+                ? "border-red-600 hover:border-red-600 w-full"
+                : "w-full"
+            }
+          />
+        </IconField>
+        {errors.firstName && (
+          <small className="text-red-600">
+            {errors.firstName.type === "required" &&
+              "The first name is required."}
+            {errors.firstName.type === "minLength" &&
+              "The first name must be at least 3 characters."}
+          </small>
+        )}
       </div>
+
       <div className="flex flex-col gap-2">
         <label htmlFor="lastName">Last Name</label>
-        <InputText id="lastName" type="text" {...register("lastName")} />
+        <IconField>
+          {errors.lastName?.type === "required" && (
+            <InputIcon
+              className="pi pi-exclamation-circle"
+              style={{ color: "red" }}
+            />
+          )}
+          <InputText
+            id="lastName"
+            type="text"
+            {...register("lastName", { required: true, minLength: 3 })}
+            className={
+              errors.lastName?.type === "required" ||
+              errors.lastName?.type === "minLength"
+                ? "border-red-600 hover:border-red-600 w-full"
+                : "w-full"
+            }
+          />
+        </IconField>
+        {errors.lastName && (
+          <small className="text-red-600">
+            {errors.lastName.type === "required" &&
+              "The last name is required."}
+            {errors.lastName.type === "minLength" &&
+              "The last name must be at least 3 characters."}
+          </small>
+        )}
       </div>
       <div className="flex flex-col gap-2">
         <label htmlFor="age">Age</label>
-        <InputText id="age" type="number" {...register("age")} />
+        <IconField>
+          {errors.age?.type === "required" && (
+            <InputIcon
+              className="pi pi-exclamation-circle"
+              style={{ color: "red" }}
+            />
+          )}
+          <InputText
+            id="age"
+            type="text"
+            {...register("age", {
+              required: true || "Age is required.",
+              maxLength: 2,
+              pattern: /^[1-9]\d*$/,
+            })}
+            className={
+              errors.age?.type === "required" ||
+              errors.age?.type === "pattern" ||
+              errors.age?.type === "maxLength"
+                ? "border-red-600 hover:border-red-600 w-full"
+                : "w-full"
+            }
+          />
+        </IconField>
+        {errors.age && (
+          <small className="text-red-600">
+            {errors.age.type === "required" && "Age is required"}
+            {errors.age.type === "maxLength" && "Invalid Input Age"}
+            {errors.age.type === "pattern" && "Invalid Input Age"}
+          </small>
+        )}
       </div>
+
       <div className="flex justify-between items-end">
         <div className="flex flex-col gap-2">
           <label htmlFor="gender">Gender</label>
           <Controller
             name="gender"
             control={control}
+            rules={{ required: true }}
             render={({ field }) => (
               <Dropdown
                 id="gender"
@@ -67,9 +144,17 @@ const ExpenseForm = () => {
                 onChange={(e) => field.onChange(e.value)}
                 options={genderOption}
                 placeholder="Select"
+                className={
+                  errors.gender?.type === "required"
+                    ? "border-red-600 hover:border-red-600"
+                    : ""
+                }
               />
             )}
           />
+          {errors.gender?.type === "required" && (
+            <small className="text-red-600">Gender is required </small>
+          )}
         </div>
         <div>
           <Button label="Submit New User" type="submit" />
