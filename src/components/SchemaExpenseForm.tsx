7fbc7e98b9f23bbc9210e52b24a84962ded6249f
Kyle Lerio <kylelerio15@gmail.com>
2024-07-04 18:55:27 +0800

    Build a toast component

--- src/components/SchemaExpenseForm.tsx
+++ src/components/SchemaExpenseForm.tsx
@@ -9,6 +9,8 @@ import ErrorMessage from "./ErrorMessage";
 import { IconField } from "primereact/iconfield";
 import { InputIcon } from "primereact/inputicon";
 import axios from "axios";
+import { useRef } from "react";
+import { Toast } from "primereact/toast";
 
 const genderOption = [
   { label: "Male", value: Gender.Male },
@@ -27,118 +29,129 @@ const ExpenseForm = () => {
   } = useForm<FormData>({
     resolver: zodResolver(schema),
   });
+  const toast = useRef<Toast>(null);
 
   const onSubmit = async (data: FormData) => {
     try {
       await axios.post("http://localhost:3000/users", data);
       reset();
+      showSuccess();
     } catch (error) {
       console.log(error, "Unpected error occured");
     }
   };
 
+  const showSuccess = () => {
+    toast.current?.show({
+      severity: "success",
+      summary: "Success",
+      detail: "Successfully added a New User!",
+      life: 3000,
+    });
+  };
+
   return (
-    <form
-      className="card flex flex-col gap-3"
-      onSubmit={handleSubmit(onSubmit)}
-    >
-      <div className="flex flex-col gap-2">
-        <label htmlFor="firstName">First Name</label>
-        <IconField>
-          {errors.firstName && (
-            <InputIcon
-              className="pi pi-exclamation-circle"
-              style={{ color: "red" }}
-            />
-          )}
-          <InputText
-            id="firstName"
-            type="text"
-            {...register("firstName")}
-            className={
-              errors.firstName
-                ? "border-red-600 hover:border-red-600 w-full"
-                : "w-full"
-            }
-          />
-        </IconField>
-        <ErrorMessage>{errors.firstName?.message}</ErrorMessage>
-      </div>
-
-      <div className="flex flex-col gap-2">
-        <label htmlFor="lastName">Last Name</label>
-        <IconField>
-          {errors.lastName && (
-            <InputIcon
-              className="pi pi-exclamation-circle"
-              style={{ color: "red" }}
-            />
-          )}
-          <InputText
-            id="lastName"
-            type="text"
-            {...register("lastName")}
-            className={
-              errors.lastName
-                ? "border-red-600 hover:border-red-600 w-full"
-                : "w-full"
-            }
-          />
-        </IconField>
-        <ErrorMessage>{errors.lastName?.message}</ErrorMessage>
-      </div>
-
-      <div className="flex flex-col gap-2">
-        <label htmlFor="age">Age</label>
-        <IconField>
-          {errors.age && (
-            <InputIcon
-              className="pi pi-exclamation-circle"
-              style={{ color: "red" }}
-            />
-          )}
-          <InputText
-            id="age"
-            type="numer"
-            {...register("age", {
-              valueAsNumber: true,
-            })}
-            className={
-              errors.age
-                ? "border-red-600 hover:border-red-600 w-full"
-                : "w-full"
-            }
-          />
-        </IconField>
-        <ErrorMessage>{errors.age?.message}</ErrorMessage>
-      </div>
-
-      <div className="flex justify-between items-end">
+    <div>
+      <Toast ref={toast} />
+      <form
+        className="card flex flex-col gap-3"
+        onSubmit={handleSubmit(onSubmit)}
+      >
         <div className="flex flex-col gap-2">
-          <label htmlFor="gender">Gender</label>
-          <Controller
-            name="gender"
-            control={control}
-            render={({ field }) => (
-              <Dropdown
-                id="gender"
-                value={field.value}
-                onChange={(e) => field.onChange(e.value)}
-                options={genderOption}
-                placeholder="Select"
-                className={
-                  errors.gender ? "border-red-600 hover:border-red-600" : ""
-                }
+          <label htmlFor="firstName">First Name</label>
+          <IconField>
+            {errors.firstName && (
+              <InputIcon
+                className="pi pi-exclamation-circle"
+                style={{ color: "red" }}
               />
             )}
-          />
-          <ErrorMessage>{errors.gender?.message}</ErrorMessage>
+            <InputText
+              id="firstName"
+              type="text"
+              {...register("firstName")}
+              className={
+                errors.firstName
+                  ? "border-red-600 hover:border-red-600 w-full"
+                  : "w-full"
+              }
+            />
+          </IconField>
+          <ErrorMessage>{errors.firstName?.message}</ErrorMessage>
         </div>
-        <div>
-          <Button label="Submit New User" type="submit" />
+        <div className="flex flex-col gap-2">
+          <label htmlFor="lastName">Last Name</label>
+          <IconField>
+            {errors.lastName && (
+              <InputIcon
+                className="pi pi-exclamation-circle"
+                style={{ color: "red" }}
+              />
+            )}
+            <InputText
+              id="lastName"
+              type="text"
+              {...register("lastName")}
+              className={
+                errors.lastName
+                  ? "border-red-600 hover:border-red-600 w-full"
+                  : "w-full"
+              }
+            />
+          </IconField>
+          <ErrorMessage>{errors.lastName?.message}</ErrorMessage>
         </div>
-      </div>
-    </form>
+        <div className="flex flex-col gap-2">
+          <label htmlFor="age">Age</label>
+          <IconField>
+            {errors.age && (
+              <InputIcon
+                className="pi pi-exclamation-circle"
+                style={{ color: "red" }}
+              />
+            )}
+            <InputText
+              id="age"
+              type="numer"
+              {...register("age", {
+                valueAsNumber: true,
+              })}
+              className={
+                errors.age
+                  ? "border-red-600 hover:border-red-600 w-full"
+                  : "w-full"
+              }
+            />
+          </IconField>
+          <ErrorMessage>{errors.age?.message}</ErrorMessage>
+        </div>
+        <div className="flex justify-between items-end">
+          <div className="flex flex-col gap-2">
+            <label htmlFor="gender">Gender</label>
+            <Controller
+              name="gender"
+              control={control}
+              render={({ field }) => (
+                <Dropdown
+                  id="gender"
+                  value={field.value}
+                  onChange={(e) => field.onChange(e.value)}
+                  options={genderOption}
+                  placeholder="Select"
+                  className={
+                    errors.gender ? "border-red-600 hover:border-red-600" : ""
+                  }
+                />
+              )}
+            />
+            <ErrorMessage>{errors.gender?.message}</ErrorMessage>
+          </div>
+          <div>
+            <Button label="Submit New User" type="submit" />
+          </div>
+        </div>
+      </form>
+    </div>
   );
 };
 
